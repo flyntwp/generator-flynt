8f6ba840bd1f17696defb8bc8243315e48e2fae6
Dominik Biedebach <dominik.biedebach@gmail.com>
2017-04-10 18:27:13 +0200

    feat(PHP Template): Added new Template for PHP Indentation

--- generators/app/templates/Components/functions.php
+++ generators/app/templates/Components/functions.php
@@ -5,9 +5,9 @@ namespace Flynt\Components\<%= elementName %>;
 use Flynt\Features\Components\Component;
 
 add_action('wp_enqueue_scripts', function () {
-  Component::enqueueAssets('<%= elementName %>');
+    Component::enqueueAssets('<%= elementName %>', []);
 });
 
 add_filter('Flynt/addComponentData?name=<%= elementName %>', function ($data) {
-  return $data;
+    return $data;
 });
