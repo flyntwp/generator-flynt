06ba04d863f8c2cf298ece44d496431b1828bcf8
Doğa Gürdal <Qakulukiam@users.noreply.github.com>
2017-08-29 13:37:51 +0200

    feat(generators/component): add option for single file generation (#16)
    * chore(generators/component, .gitignore): remove deprecated TODOs, add .vscode to ignored dirs
    
    * feat(generators/component): add single file adding functionality, refactor template structure
    
    removed custom sub directory since it won't be needed anymore
    
    * fix(generators/component): use case-insensitive file comparison for file existence check
    
    README.md vs readme.md, probably only an OSX issue
    
    * chore(generators/component): remove refactor TODO
    
    not needed for now, will reconsider in case more complexity is added in the future

--- generators/component/index.js
+++ generators/component/index.js
@@ -1,6 +1,7 @@
 const Generator = require('yeoman-generator')
 const _ = require('lodash')
 const categories = require('./data/categories.js')
+const fs = require('fs')
 
 module.exports = class extends Generator {
   constructor(args, opts) {
@@ -13,6 +14,38 @@ module.exports = class extends Generator {
   }
 
   prompting () {
+    return this.prompt([
+      {
+        type: 'list',
+        name: 'type',
+        message: 'Please select your desired action:',
+        choices: [
+          {
+            name: 'Create a custom component from scratch',
+            value: 'custom'
+          },
+          {
+            name: 'Add a template file to an existing component',
+            value: 'file'
+          }
+        ]
+      }
+    ]).then((answers) => {
+      this.type = answers.type
+      if (this.type === 'custom') {
+        return this._promptCustomComponent().then((answers) => {
+          this.name = _.upperFirst(answers.name)
+          if (answers.category) {
+            this.name = answers.category + this.name
+          }
+        })
+      } else if (this.type === 'file') {
+        return this._promptComponentFile()
+      }
+    })
+  }
+
+  _promptCustomComponent() {
     return this.prompt([
       {
         type: 'list',
@@ -31,36 +64,73 @@ module.exports = class extends Generator {
           return true
         }
       }
-    ]).then((answers) => {
-      let name = _.upperFirst(answers.name)
+    ])
+  }
 
-      // TODO: use base component selection and auto prepend that component's category, use this for custom components only
-      if (answers.category) {
-        name = answers.category + name
+  _promptComponentFile() {
+    const templateFiles = fs.readdirSync(this.templatePath())
+    const components = fs.readdirSync(this.destinationPath('Components'))
+      .filter(file => fs.statSync(this.destinationPath(`Components/${file}`)).isDirectory())
+      .reduce((carry, component) => {
+        const existingFiles = fs.readdirSync(this.destinationPath(`Components/${component}`))
+        const validFiles = _.differenceWith(
+          templateFiles,
+          existingFiles,
+          (a, b) => a.toLowerCase() === b.toLowerCase()
+        )
+        if (validFiles.length) {
+          carry[component] = validFiles
+        }
+        return carry
+      }, [])
+    return this.prompt([
+      {
+        type: 'list',
+        name: 'name',
+        message: 'Select a component to add files to',
+        choices: Object.keys(components)
       }
-
-      this.namePretty = _.startCase(name)
-      this.nameKebabCase = _.kebabCase(name)
-      this.nameLowerCamelCase = _.camelCase(name)
-      this.nameUpperCamelCase = _.upperFirst(this.nameLowerCamelCase)
-
-      // TODO: prompt for this template (base components vs. custom component)
-      this.template = 'custom'
+    ]).then((answers) => {
+      this.name = answers.name
+      return this.prompt([
+        {
+          type: 'checkbox',
+          name: 'files',
+          message: 'Select the file(s) you would like to generate',
+          choices: components[answers.name],
+          validate: (answer) => answer.length > 0 || 'Please choose at least one file to add.'
+        }
+      ])
+    }).then((answers) => {
+      this.files = answers.files
     })
   }
 
   writing () {
-    this.log('Creating files...')
+    this.namePretty = _.startCase(this.name)
+    this.nameKebabCase = _.kebabCase(this.name)
+    this.nameLowerCamelCase = _.camelCase(this.name)
+    this.nameUpperCamelCase = _.upperFirst(this.nameLowerCamelCase)
 
+    if (this.type === 'custom') {
+      this._writeCustomComponent()
+    } else {
+      this._writeComponentFile()
+    }
+  }
+
+  _writeCustomComponent() {
+    this.log('Creating files...')
+    
     const destDir = `Components/${this.nameUpperCamelCase}/`
 
     this.fs.copy(
-      this.templatePath(`${this.template}/*.jpg`),
+      this.templatePath(`*.jpg`),
       this.destinationPath(destDir)
     )
 
     this.fs.copyTpl(
-      this.templatePath(`${this.template}/!(*.jpg)`),
+      this.templatePath(`!(*.jpg)`),
       this.destinationPath(destDir),
       {
         namePretty: this.namePretty,
@@ -71,8 +141,39 @@ module.exports = class extends Generator {
     )
   }
 
+  _writeComponentFile() {
+    this.log(`Adding ${this.files.join(', ')} to ${this.name}...`)
+
+    const nonTemplateFiles = this.files.filter(file => file.endsWith('.jpg'))
+    const templateFiles = _.difference(this.files, nonTemplateFiles)
+    const destDir = `Components/${this.name}/`
+
+    if (nonTemplateFiles.length) {
+      this.fs.copy(
+        this.templatePath(`*(${nonTemplateFiles.join('|')})`),
+        this.destinationPath(destDir)
+      )
+    }
+
+    if (templateFiles.length) {
+      this.fs.copyTpl(
+        this.templatePath(`*(${templateFiles.join('|')})`),
+        this.destinationPath(destDir),
+        {
+          namePretty: this.namePretty,
+          nameKebabCase: this.nameKebabCase,
+          nameUpperCamelCase: this.nameUpperCamelCase,
+          nameLowerCamelCase: this.nameLowerCamelCase
+        }
+      )
+    }
+  }
+
   end () {
-    // TODO: do some checks to be sure that everything went well
-    this.log(`Successfully created component: ${this.nameUpperCamelCase}`)
+    if (this.type === 'custom') {
+      this.log(`Successfully created component: ${this.nameUpperCamelCase}`)
+    } else if (this.type === 'file') {
+      this.log('File generation successful')
+    }
   }
 }
