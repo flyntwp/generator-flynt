c3ba7d08574d991199485906f80e58706ef091cb
Doğa Gürdal <doga@bleech.de>
2018-05-17 11:41:22 +0200

    feat(generators/component): move asset enqueuing into addComponentData
    This allows limiting the asset requests to the pages where the component is in the construction plan. When needed, the component assets can still be enqueued in parent components, where they may be loaded asynchronously.

--- generators/component/templates/functions.php
+++ generators/component/templates/functions.php
@@ -4,11 +4,10 @@ namespace Flynt\Components\<%= nameUpperCamelCase %>;
 
 use Flynt\Features\Components\Component;
 
-add_action('wp_enqueue_scripts', function () {
-    Component::enqueueAssets('<%= nameUpperCamelCase %>');
-});
-
 add_filter('Flynt/addComponentData?name=<%= nameUpperCamelCase %>', function ($data) {
+    add_action('wp_enqueue_scripts', function () {
+        Component::enqueueAssets('<%= nameUpperCamelCase %>');
+    });
 
     return $data;
 });
