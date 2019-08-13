a6e9efcf6c4824d26880150688e3a858a66c72aa
Doğa Gürdal <doga@bleech.de>
2019-08-13 16:58:42 +0200

    feat(generators/component): use new Api class to register fields

--- generators/component/templates/functions.php
+++ generators/component/templates/functions.php
@@ -3,13 +3,14 @@
 namespace Flynt\Components\<%= nameUpperCamelCase %>;
 
 use Flynt;
+use Flynt\Api;
 use Flynt\Utils\Options;
 
 add_filter('Flynt/addComponentData?name=<%= nameUpperCamelCase %>', function ($data) {
     return $data;
 });
 
-// Flynt\registerFields('<%= nameUpperCamelCase %>', [
+// Api::registerFields('<%= nameUpperCamelCase %>', [
 //     'layout' => [
 //         'name' => '<%= nameLowerCamelCase %>',
 //         'label' => '<%= namePrettySplit %>',
