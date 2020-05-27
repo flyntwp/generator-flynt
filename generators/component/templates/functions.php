fb4e5c240e36abe733480781a5b14a6533adb2a8
Roel Tinkhof <roel@bleech.de>
2020-05-27 10:40:15 +0200

    feat(generators/component): Changed the functions to new Flynt setup

--- generators/component/templates/functions.php
+++ generators/component/templates/functions.php
@@ -2,49 +2,27 @@
 
 namespace Flynt\Components\<%= nameUpperCamelCase %>;
 
-use Flynt\Api;
 use Flynt\Utils\Options;
 
 add_filter('Flynt/addComponentData?name=<%= nameUpperCamelCase %>', function ($data) {
     return $data;
 });
 
-Api::registerFields('<%= nameUpperCamelCase %>', [
-    'layout' => [
-        'name' => '<%= nameLowerCamelCase %>',
-        'label' => '<%= namePrettySplit %>',
+function getACFLayout()
+{
+    return [
+        'name' => 'GridCards',
+        'label' => 'Grid: Cards',
         'sub_fields' => [
-            [
-                'label' => 'General',
-                'name' => 'generalTab',
-                'type' => 'tab',
-                'placement' => 'top',
-                'endpoint' => 0,
-            ],
-            [
-                'label' => 'Options',
-                'name' => 'optionsTab',
-                'type' => 'tab',
-                'placement' => 'top',
-                'endpoint' => 0,
-            ],
-            [
-                'label' => '',
-                'name' => 'options',
-                'type' => 'group',
-                'layout' => 'row',
-                'sub_fields' => [
-                    Api::loadFields('FieldVariables', 'theme'),
-                ],
-            ],
-        ],
-    ],
-]);
+
+        ]
+    ];
+}
 
 Options::addTranslatable('<%= nameUpperCamelCase %>', [
-    
+
 ]);
 
 Options::addGlobal('<%= nameUpperCamelCase %>', [
-    
+
 ]);
