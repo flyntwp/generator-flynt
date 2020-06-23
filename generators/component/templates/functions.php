0931feeef660add0d928857351ee8ffeb9d2a54f
Roel Tinkhof <roel@bleech.de>
2020-06-23 16:09:47 +0200

    feat(functions): Added additional data to show possible settings

--- generators/component/templates/functions.php
+++ generators/component/templates/functions.php
@@ -2,6 +2,7 @@
 
 namespace Flynt\Components\<%= nameUpperCamelCase %>;
 
+use Flynt\FieldVariables;
 use Flynt\Utils\Options;
 
 add_filter('Flynt/addComponentData?name=<%= nameUpperCamelCase %>', function ($data) {
@@ -11,10 +12,32 @@ add_filter('Flynt/addComponentData?name=<%= nameUpperCamelCase %>', function ($d
 function getACFLayout()
 {
     return [
-        'name' => 'GridCards',
-        'label' => 'Grid: Cards',
+        'name' => '<%= nameUpperCamelCase %>',
+        'label' => '<%= namePretty %>',
         'sub_fields' => [
-
+            [
+                'label' => 'General',
+                'name' => 'generalTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0,
+            ],
+            [
+                'label' => 'Options',
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0,
+            ],
+            [
+                'label' => '',
+                'name' => 'options',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\getTheme()
+                ],
+            ],
         ]
     ];
 }
